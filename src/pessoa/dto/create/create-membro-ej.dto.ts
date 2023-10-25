78508d3d8d61014b0482d44ca2ba5db84a9b5e7f
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2023-10-25 19:14:28 -0300

    Update create-membro-ej.dto.ts

--- src/pessoa/dto/create/create-membro-ej.dto.ts
+++ src/pessoa/dto/create/create-membro-ej.dto.ts
@@ -1,1 +1,16 @@
-export class CreateMembroEjDto {}
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateMembroEjDto {
+  @IsNotEmpty()
+  @IsString()
+  matriculaInstituicao: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  dataFiliacao: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  enderecoResidencial: string;
+  RG: string;
+}
